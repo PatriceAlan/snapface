d2bfc860f83f1ea4f13c658910c735860ad6d7b8
PatriceAlan <peckpatricealan@gmail.com>
2025-11-28 22:00:42 +0100

    Added auth guards to handle login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,35 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     { 
         path: 'facesnaps/:id',
         loadComponent: () =>
-        import('./facesnaps-components/single-face-snap/single-face-snap.component')
-            .then(m => m.SingleFaceSnapComponent)
+            import('./facesnaps-components/single-face-snap/single-face-snap.component')
+                .then(m => m.SingleFaceSnapComponent),
+        canActivate: [AuthGuard]
     },
     { 
         path: 'facesnaps',
         loadComponent: () =>
-        import('./facesnaps-components/face-snap-list/face-snap-list.component')
-            .then(m => m.FaceSnapListComponent)
+            import('./facesnaps-components/face-snap-list/face-snap-list.component')
+                .then(m => m.FaceSnapListComponent),
+        canActivate: [AuthGuard]
     },
     { 
         path: 'create',
         loadComponent: () =>
-        import('./facesnaps-components/new-face-snap/new-face-snap.component')
-            .then(m => m.NewFaceSnapComponent)
+            import('./facesnaps-components/new-face-snap/new-face-snap.component')
+                .then(m => m.NewFaceSnapComponent),
+        canActivate: [AuthGuard]
+        
     },
     { 
         path: '',
         loadComponent: () =>
-        import('./landing-page/landing-page.component')
-            .then(m => m.LandingPageComponent)
+            import('./landing-page/landing-page.component')
+                .then(m => m.LandingPageComponent),
+        canActivate: [AuthGuard]
     },
     {
         path: 'auth',
